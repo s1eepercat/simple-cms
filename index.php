b6eaf0e6f061eaeaca1ab7158cfca24ceaa8cbe6
Aleksandr Jevlentjev <jevlentjev@gmail.com>
2019-12-14 18:41:08 +0200

    sessions, cookies

--- index.php
+++ index.php
@@ -26,16 +26,14 @@ if ($results === false) {
 <?php require 'includes/header.php'; ?>
 
 
-<?php if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']) : ?>
+<?php if (isLoggedIn()) : ?>
 	<p>You are logged in. <a href="logout.php">Log out</a> </p>
-
+	<p><a href = "new-article.php">New article</a></p>
 <?php else: ?>
 	<p>You are not logged in. <a href="login.php">Log in</a> </p>
-<?php endif; ?>
+<?php endif; ?> 
 
-
-<a href = "new-article.php">New article</a>
-
+ 
 			<?php if (empty($articles)): ?>
 				<p>No articles found.</p>
 			<?php else: ?>
